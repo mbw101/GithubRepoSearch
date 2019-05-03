11e84358b1567c6153b4e5a4d21bb1c3589267e6
Malcolm Wright <wrightmbw01@gmail.com>
2019-05-03 17:49:21 -0400

    Added menu with search button and a toast

--- app/src/main/java/com/example/githubreposearch/MainActivity.java
+++ app/src/main/java/com/example/githubreposearch/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.githubreposearch;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     EditText searchBoxEditText;
     TextView urlDisplayTextView;
     TextView searchResults;
+
+    final static String GIT_HUB_BASE_URL =
+            "https://api.github.com/search/repositories";
+    final static String PARAM_QUERY = "q";
+    final static String PARAM_SORT = "sort";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
         searchBoxEditText = findViewById(R.id.et_search_box);
         urlDisplayTextView = findViewById(R.id.urlDisplay);
         searchResults = findViewById(R.id.githubSearchResultsJson);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+
+        // check to see if the search button was the item pressed
+        if (menuItemThatWasSelected == R.id.action_search)
+        {
+            Context context = MainActivity.this;
+            String message = "Search Clicked";
+
+            // show a toast
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
